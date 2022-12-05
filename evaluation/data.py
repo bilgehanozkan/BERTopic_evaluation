01fecaa5053a64af5ef197eaf352c3eba5b1570d
bilgehanozkan <101361071+bilgehanozkan@users.noreply.github.com>
2022-12-05 16:13:13 +0300

    Update data.py

--- evaluation/data.py
+++ evaluation/data.py
@@ -183,7 +183,7 @@ class DataLoader:
     def _trump(self) -> Tuple[List[str], List[str]]:
         """Prepare the trump dataset"""
         trump = pd.read_csv(
-            "C:/Users\blghn/Desktop/Veriseti_dropna/birlestirilmis/total.csv", encoding ="cp1252"
+            "C:/Users/blghn/Desktop/Veriseti_dropna/birlestirilmis/total.csv", encoding ="cp1252"
         )
         trump = trump.loc[(trump.comment != ""), :]
         timestamps = trump.date.to_list()
